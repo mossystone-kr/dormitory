27a786f41b2adb5ca742e150f6418db5ac4787ae
Malloc314159 <65201256+Malloc314159@users.noreply.github.com>
2023-07-10 13:59:24 +0900

    Design (1)

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# 히히 야밤에 배고프당...
+# 라이브러리 임포트
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas
+
+# 엑셀 파일로 학생 정보+자습실 배치, 방 배치 등 입력
+
+
+# 배치
+
+
+# UI 디자인
+
+
+# 배치 내보내기
